8b10ae25cea87d81f11030a8a19afffb9ec4e59a
Marcos <marcosrio.26@gmail.com>
2024-03-17 23:41:14 -0300

    Funciona

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { TaskMOdule } from './task/task.module';
 import { TasksController } from './task/task.controllers';
 import { DatabaseModule } from './task/dataBase/dataBase.module';
 import { TaskService } from './task/task.service';
+import { taskProviders } from './task/task.providers';
 
 @Module({
   imports: [TaskMOdule, DatabaseModule],
   controllers: [TasksController],
-  providers: [TaskService],
+  providers: [TaskService, ...taskProviders],
 })
 export class AppModule {}
